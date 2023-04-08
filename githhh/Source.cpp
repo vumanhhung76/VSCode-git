7bd4d935557b856d507f2f53b01ce43072af6925
vumanhhung76 <hung.vm181513@gmail.com>
2023-04-08 12:52:36 +0700

    con4

--- githhh/Source.cpp
+++ githhh/Source.cpp
@@ -5,17 +5,17 @@ int giaiPT(float a, float b, float c, float& x1, float& x2) {
 	float delta = b * b - 4 * a * c;
 	if (delta < 0) {
 		x1 = x2 = 0.0;
-		return 0;
+		return 4;
 	}
 	else if (delta == 0) {
 		x1 = x2 = -b / (2 * a);
-		return 2;
+		return 1;
 	}
 	else {
 		delta = sqrt(delta);
 		x1 = (-b + delta) / (2 * a);
 		x2 = (-b - delta) / (2 * a);
-		return 1;
+		return 2;
 	}
 }
 int main() {
